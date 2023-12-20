39ccfa75115195bf24149bd131fe5c06e13900d9
weiju <weiju.wu@gmail.com>
2023-12-20 08:38:45 -0800

    rearranged files into scripts and database folders

---
+++ scripts/annotate_mtb_results.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+
+import pandas
+import argparse
+
+MTB_GENOME_SIZE = 4411532
+
+def read_databases():
+    # 1. read Tuberculist information
+    header = ['GeneID', 'GeneName', 'Strand', 'Start', 'Stop', 'Description', 'Category']
+    tuberculist_df = pandas.read_csv('2_Tuberculist_new_20150307', sep='\t',
+                                     header=None, names=header)
+    gene_infos = {}
+    gene_ids = []
+
+    igr = {}
+    igr_keys = []
+
+    for index, row in tuberculist_df.iterrows():
+        gene_ids.append(row['GeneID'])
+        gene_infos[row['GeneID']] = {
+            "name": row["GeneName"],
+            "strand": row["Strand"],
+            "start": row["Start"],
+            "stop": row["Stop"],
+            "description": row["Description"],
+            "category": row["Category"]
+        }
+    # we have the gene_infos map, now build igr
+    for index, row in tuberculist_df.iterrows():
+        if index > 0:
+            if row["Start"] > tuberculist_df.iloc[index - 1]["Stop"]:
+                prev_gene = tuberculist_df.iloc[index - 1]["GeneID"]
+                igr_key = "%s-%s" % (prev_gene, row["GeneID"])
+                igr[igr_key] = {
+                    "start": gene_infos[prev_gene]["stop"] + 1,
+                    "stop": row['Start'] - 1
+                }
+                igr_keys.append(igr_key)
+    """
+    # validate against jigr
+    jigr_df = pandas.read_csv('jigr.tsv', sep='\t', header=None, names=['key', 'start', 'stop'])
+    jigr_map = {}
+    for index, row in jigr_df.iterrows():
+        jigr_map[row['key']] = {
+            'start': row['start'], 'stop': row['stop']
+        }
+    for key, pos1 in jigr_map.items():
+        pos2 = igr[key]
+        if pos1 != pos2:
+            raise Exception("key: %s, pos in Perl %s != Python %s" % (key, str(pos1), str(pos2)))
+    """
+
+    # 2. read Codon -> AA mapping
+    code = {}
+    genetic_codes_df = pandas.read_csv('3_genetic_codes', sep='\t',
+                                       header=None, names=['Codon', 'Result'])
+    for index, row in genetic_codes_df.iterrows():
+        code[row['Codon']] = row['Result']
+
+    # 3. Read genome
+    genome = ""
+    with open('4_mtbc_sequence.fasta') as infile:
+        for line in infile:
+            if not line.startswith(">"):
+                genome += line.strip()
+
+    return gene_ids, gene_infos, code, genome, igr_keys, igr
+
+DESCRIPTION = """annotate_mtb_results - Annotate SNP results with Tuberculist information"""
+
+def is_in_gene(pos, gene_info):
+    return pos >= gene_info['start'] and pos <= gene_info['stop']
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description=DESCRIPTION)
+    parser.add_argument('snpfile', help="SNP result file")
+    parser.add_argument('varscan', help="VarScan result file")
+    args = parser.parse_args()
+
+    freq_map = {}
+    varscan_df = pandas.read_csv(args.varscan, sep='\t')
+    for index, row in varscan_df.iterrows():
+        chrom = row["Chrom"]
+        pos = row["Position"]
+        special = row["Cons:Cov:Reads1:Reads2:Freq:P-value.1"]
+        freq_map[pos] = special.split(":")[4]
+
+    gene_ids, gene_infos, code, genome, igr_keys, igr = read_databases()
+    snp_df = pandas.read_csv(args.snpfile, sep='\t', header=None,
+                             names=["VarscanPos", "Ref", "Alt"])
+    header = ["VarscanPosition", "Ref", "Alt", "CodonPos", "Type_WTAA_MutAA",
+              "WTCodon_MutCodon", "Gene ID", "Name",
+              "Description", "Type", "Frequency"]
+    print('\t'.join(header))
+    for index, row in snp_df.iterrows():
+        freq = freq_map[row['VarscanPos']]
+        for gene_id in gene_ids:
+            gene_info = gene_infos[gene_id]
+            if is_in_gene(row['VarscanPos'], gene_info):
+                if gene_id.startswith('MTB'):
+                    print("%s\t%s\t%s\t-\t---\t---\t%s\t%s\t%s\t%s\t%s" %
+                          (row["VarscanPos"], row["Ref"], row["Alt"],
+                           gene_id,
+                           gene_info['name'], gene_info['description'], gene_info['category'],
+                           freq))
+                else:
+                    if gene_info['strand'] == '+':
+                        #print("NON-MTB GENE (+) %s\n" % gene_id);
+                        length = gene_info['stop'] - gene_info['start'] + 1
+                        seq = genome[gene_info['start'] - 1:(gene_info['start'] -  1) + length]
+                        loci = row["VarscanPos"] - gene_info['start'] + 1
+                        ct = int(loci / 3)
+                        remain = loci % 3
+                        if remain == 0:
+                            codon = ct
+                            wildtype = seq[loci - 3: (loci - 3) + 3]
+                            mutation = wildtype[:2] + row['Alt']
+                        elif remain == 1:
+                            codon = ct + 1
+                            wildtype = seq[loci - 1: (loci - 1) + 3]
+                            mutation = row['Alt'] + wildtype[1:3]
+                        elif remain == 2:
+                            codon = ct + 1
+                            wildtype = seq[loci - 2: (loci - 2) + 3]
+                            mutation = wildtype[0] + row['Alt'] + wildtype[2]
+                    elif gene_info['strand'] == '-':
+                        #print("NON-MTB GENE (-): %s\n" % gene_id);
+                        length = gene_info['stop'] - gene_info['start'] + 1
+                        seq = genome[gene_info['start'] - 1:(gene_info['start'] -  1) + length]
+                        seq = seq[::-1]  # reverse the sequence
+                        loci = gene_info['stop'] - row['VarscanPos'] + 1
+                        ct = int(loci / 3)
+                        remain = loci % 3
+                        if remain == 0:
+                            codon = ct
+                            wildtype = seq[loci - 3: (loci - 3) + 3]
+                            mutation = wildtype[0:2] + row['Alt']
+                        elif remain == 1:
+                            codon = ct + 1
+                            wildtype = seq[loci - 1: (loci - 1) + 3]
+                            mutation = row['Alt'] + wildtype[1:3]
+                        elif remain == 2:
+                            codon = ct + 1
+                            wildtype = seq[loci - 2: (loci - 2) + 3]
+                            mutation = wildtype[0] + row['Alt'] + wildtype[2]
+
+                        # translate bases
+                        wildtype = wildtype.translate(str.maketrans("ATGC", "TACG"))
+                        mutation = mutation.translate(str.maketrans("ATGC", "TACG"))
+
+                    if code[wildtype] == code[mutation]:
+                        restype = "Synonymous"
+                    else:
+                        restype = "Nonsynonymous"
+
+                    code_info = '%s-%s-%s' % (restype, code[wildtype], code[mutation])
+                    triplets = '%s-%s' % (wildtype, mutation)
+                    print("%s\t%s\t%s\t%d\t%s\t%s\t%s\t%s\t%s\t%s\t%s" %
+                          (row["VarscanPos"], row["Ref"], row["Alt"],
+                           codon, code_info, triplets, gene_id, gene_info['name'],
+                           gene_info['description'], gene_info['category'], freq))
+
+            else:
+                #print("not a gene location")
+                pass
+
+        # II. Iterate over joined genes
+        for j in igr_keys:
+            j_info = igr[j]
+            if row['VarscanPos'] >= j_info['start'] and row['VarscanPos'] <= j_info['stop']:
+                gene1, gene2 = j.split('-')
+                if gene1 == 'Rv3924c' and gene2 == 'Rv0001':
+                    left = row['VarscanPos'] - j_info['start']
+                    right = MTB_GENOME_SIZE - row['VarscanPos'];
+                else:
+                    left = row['VarscanPos'] - gene_infos[gene1]['stop']
+                    right= gene_infos[gene2]['start'] - row['VarscanPos']
+                gene_info1 = gene_infos[gene1]
+                gene_info2 = gene_infos[gene2]
+                loc_str = "%s%d-%d%s" % (gene_info1['strand'], left, right, gene_info2['strand'])
+                name_str = "%s-%s" % (gene_info1['name'], gene_info2['name'])
+                desc_str = "%s##%s" % (gene_info1['description'], gene_info2['description'])
+                cat_str = "%s##%s" % (gene_info1['category'], gene_info2['category'])
+                print("%d\t%s\t%s\t-\t---\t%s\t%s\t%s\t%s\t%s\t%s" %
+                      (row['VarscanPos'], row['Ref'], row['Alt'],
+                       loc_str, j, name_str, desc_str, cat_str, freq))
---
+++ scripts/make_snp_calling.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+
+import argparse
+import jinja2
+import os
+from globalsearch.rnaseq.find_files import find_fastq_files
+
+DESCRIPTION = "make_fixed_snp_calling"
+
+FIXED_TEMPLATE = """#!/bin/bash
+sickle pe -l 35 -f {{fastq1}} -r {{fastq2}} -t sanger -o {{trimmed1}} -p {{trimmed2}} -s {{trimmedS}}
+bwa aln -R 1 {{fasta_path}} {{trimmed1}} > {{sai1}}
+bwa aln -R 1 {{fasta_path}} {{trimmed2}} > {{sai2}}
+bwa aln -R 1 {{fasta_path}} {{trimmedS}} > {{saiS}}
+bwa sampe -a 1000 -n 1 -N 1 {{fasta_path}} {{sai1}} {{sai2}} {{trimmed1}} {{trimmed2}} > {{paired_sam}}
+bwa samse -n 1 {{fasta_path}} {{saiS}} {{trimmedS}} > {{single_sam}}
+samtools view -bhSt {{fasta_path}}.fai {{paired_sam}} -o {{paired_bam}}
+samtools view -bhSt {{fasta_path}}.fai {{single_sam}} -o {{single_bam}}
+samtools merge {{merged_bam}} {{paired_bam}} {{single_bam}}
+samtools sort {{merged_bam}} -o {{sorted_bam}}
+samtools index {{sorted_bam}}
+samtools mpileup -q 30 -Q 20 -ABOf {{fasta_path}} {{sorted_bam}} > {{pileup_file}}
+java -jar {{varscan_path}} mpileup2snp {{pileup_file}} --min-coverage 3 --min-reads2 2 --min-avg-qual 20 --min-var-freq 0.01 --min-freq-for-hom 0.9 --p-value 99e-02 --strand-filter 1 > {{varscan_file}}
+java -jar {{varscan_path}} mpileup2cns {{pileup_file}} --min-coverage 3 --min-avg-qual 20 --min-var-freq 0.75 --min-reads2 2 --strand-filter 1 > {{cns_file}}
+perl {{resr_path}}/PPE_PE_INS_filt.pl {{resr_path}}/PPE_INS_loci.list {{varscan_file}} > {{ppe_file}}
+perl {{resr_path}}/format_trans.pl {{ppe_file}} > {{for_file}}
+perl {{resr_path}}/fix_extract.pl {{for_file}} > {{fix_file}}
+perl {{resr_path}}/unfix_pileup_match.pl {{for_file}} {{pileup_file}} > {{forup_file}}
+cut -f2-4 {{fix_file}} > {{snp_file}}
+"""
+
+UNFIXED_TEMPLATE="""#!/bin/bash
+
+sickle pe -l 35 -f {{fastq1}} -r {{fastq2}} -t sanger -o {{trimmed1}} -p {{trimmed2}} -s {{trimmedS}}
+bwa aln -R 1 {{fasta_path}} {{trimmed1}} > {{sai1}}
+bwa aln -R 1 {{fasta_path}} {{trimmed2}} > {{sai2}}
+bwa aln -R 1 {{fasta_path}} {{trimmedS}} > {{saiS}}
+bwa sampe -a 1000 -n 1 -N 1 {{fasta_path}} {{sai1}} {{sai2}} {{trimmed1}} {{trimmed2}} > {{paired_sam}}
+bwa samse -n 1 {{fasta_path}} {{saiS}} {{trimmedS}} > {{single_sam}}
+samtools view -bhSt {{fasta_path}}.fai {{paired_sam}} -o {{paired_bam}}
+samtools view -bhSt {{fasta_path}}.fai {{single_sam}} -o {{single_bam}}
+samtools merge {{merged_bam}} {{paired_bam}} {{single_bam}}
+samtools sort {{merged_bam}} -o {{sorted_bam}}
+samtools index {{sorted_bam}}
+
+#generate mpileup file for each sample
+samtools mpileup -q 30 -Q 20 -ABOf {{fasta_path}} {{sorted_bam}} > {{pileup_file}}
+
+#using varscan for SNP calling
+java -jar {{varscan_path}} mpileup2snp {{pileup_file}} --min-coverage 3 --min-reads2 2 --min-avg-qual 20 --min-var-freq 0.01 --min-freq-for-hom 0.9 --p-value 99e-02 --strand-filter 1 > {{varscan_file}}
+java -jar {{varscan_path}} mpileup2cns {{pileup_file}} --min-coverage 3 --min-avg-qual 20 --min-var-freq 0.75 --min-reads2 2 --strand-filter 1 > {{cns_file}}
+
+#exclude regions belonging to PPE/PE and insertion sequences, and also exclude the regions that were recently marked as error-prone (Marin, Bioinformatics, 2022)
+perl {{resr_path}}/PE_IS_filt.pl {{resr_path}}/Excluded_loci_mask.list {{varscan_file}} > {{ppe_file}}
+perl {{resr_path}}/format_trans.pl {{ppe_file}} > {{for_file}}
+
+#extract read location from mpileup file (where does a mutation allele locate on a seqeuncing read), for further filtering based on tail distribution
+perl {{resr_path}}/mix_pileup_merge.pl {{for_file}} {{pileup_file}} > {{forup_file}}
+
+#average sequencing depth, only include samples with genome coverage rate >0.9 and sequencing depth >20X
+#sed 's/:/\\t/g' {{cns_file}}|awk '{if ($6 >= 3){n++;sum+=$6}} END {print \"\\t\",n/4411532,\"\\t\",sum/n}' > {{dep_file}}
+python3 {{resr_path}}/avg_sequencing_depth.py {{cns_file}} > {{dep_file}}
+
+#extract unfixed SNPs from forup files, this will create two files: "markdisc" and "markkept"; the suspected false positives(such as mutations with tail region enrichment) will be moved to markdisc file
+perl {{resr_path}}/mix_extract_0.95.pl {{forup_file}} > {{mix_file}}
+perl {{resr_path}}/forup_format.pl {{mix_file}} > {{mixfor_file}}
+perl {{resr_path}}/info_mark.pl {{mixfor_file}} > {{mixmark_file}}
+perl {{resr_path}}/redepin_filt.pl {{resr_path}}/Excluded_loci_mask.list {{dep_file}} {{mixmark_file}}
+
+#filter list of highly repeated mutations with similar mutational frequency
+#for those unfixed mutations that arise >=5 times in the 50K isolates, further check their reliability based on 1) the ratio in "markkept"; 2) the distribution of the mutational frequency.
+cat *mixmarkkept > all_KEPT.txt
+perl {{resr_path}}/loci_freq_count.pl all_KEPT.txt > kept_repeat.txt
+cat *mixmark > all_MIX.txt
+perl {{resr_path}}/loci_freq_count.pl all_MIX.txt > mix_repeat.txt
+perl {{resr_path}}/repeat_number_merge.pl mix_repeat.txt kept_repeat.txt > merge_kept_mix.txt
+perl {{resr_path}}/ratio.pl merge_kept_mix.txt > merge_kept_mix_ratio.txt
+# Comparing to 5 can lead to 0 *per5up.txt files
+awk '$4>=5' merge_kept_mix_ratio.txt |awk '$6>0.6'|cut -f1|while read i;do echo $i > $i.per5up.txt;grep -w $i all_KEPT.txt|cut -f12 >> $i.per5up.txt;done
+paste *per5up.txt > 5up_0.6_paste.txt
+perl {{resr_path}}/stdv.pl 5up_0.6_paste.txt |awk '$2<0.25'|cut -f1 > 5up_0.6_0.25.list
+perl {{resr_path}}/freq_extract.pl 5up_0.6_0.25.list 5up_0.6_paste.txt > 5up_0.6_0.25.txt
+awk '$4>=5' merge_kept_mix_ratio.txt|cut -f1 > 5up.list
+perl {{resr_path}}/repeat_loci.pl 5up_0.6_0.25.list 5up.list > 5up_remove_loc.list
+perl {{resr_path}}/repeatloci_filter.pl 5up_remove_loc.list {{markkept_file}} > {{keptfilt_file}}
+
+#annotation of unfixed SNPs
+cut -f9-11 {{keptfilt_file}} > {{keptsnp_file}}
+perl {{resr_path}}/1_MTBC_Annotation_mtbc_4411532.pl {{keptsnp_file}} > {{keptanofilt_file}}
+"""
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description=DESCRIPTION)
+    parser.add_argument('--fastq_pattern', default="*_{{readnum}}.fastq", help="FASTQ pattern")
+    parser.add_argument('input_path', help="input path to sample")
+    args = parser.parse_args()
+    fastq_files = find_fastq_files(args.input_path, [args.fastq_pattern])
+    if len(fastq_files) == 0:
+        print("no FASTQ files found")
+        exit(1)
+    fq1, fq2 = fastq_files[0]
+    basename1 = os.path.basename(fq1)
+    basename2 = os.path.basename(fq2)
+    stem1 = basename1.replace(".fastq", "").replace("gz", "")
+    stem2 = basename2.replace(".fastq", "").replace("gz", "")
+
+    # depending on the readnum format
+    stem0 = stem1.replace("_1", "")
+    fasta_path = "/bwa_pipeline/reference/MTB_ancestor_reference.fasta"
+    resr_path = "/resR_scripts"
+    varscan_path = "/jarfiles/VarScan.v2.4.0.jar"
+
+    fixed_templ = jinja2.Template(FIXED_TEMPLATE)
+    config = {
+        "fasta_path": fasta_path, "resr_path": resr_path, "varscan_path": varscan_path,
+        "fastq1": fq1, "fastq2": fq2,
+        "trimmed1": "%s_trimmed.fq" % stem1, "trimmed2": "%s_trimmed.fq" % stem2,
+        "trimmedS": "%s_trimmedS.fq" % stem0,
+        "sai1": "%s.sai" % stem1, "sai2": "%s.sai" % stem2, "saiS": "%s_S.sai" % stem0,
+        "paired_sam": "%s.paired.sam" % stem0, "single_sam": "%s.single.sam" % stem0,
+        "paired_bam": "%s.paired.bam" % stem0, "single_bam": "%s.single.bam" % stem0,
+        "merged_bam": "%s.merged.bam" % stem0, "sorted_bam": "%s.sorted.bam" % stem0,
+        "pileup_file": "%s.pileup" % stem0, "varscan_file": "%s.varscan" % stem0,
+        "cns_file": "%s.cns" % stem0, "ppe_file": "%s.ppe" % stem0,
+        "for_file": "%s.for" % stem0, "fix_file": "%s.fix" % stem0,
+        "forup_file": "%s.forup" % stem0, "snp_file": "%s.snp" % stem0,
+        "dep_file": "%s.dep" % stem0, "mix_file": "%s.mix" % stem0,
+        "mixfor_file": "%s.mixfor" % stem0, "mixmark_file": "%s.mixmark" % stem0,
+        "markkept_file": "%s.mixmarkkept" % stem0,
+        "keptfilt_file": "%s.keptfilt" % stem0, "keptsnp_file": "%s.keptsnp" % stem0,
+        "keptanofilt_file": "%s.keptanofilt" % stem0
+    }
+    fixed_out = fixed_templ.render(config)
+    with open("fixed_snp_calling.sh", "w") as outfile:
+        outfile.write(fixed_out)
+
+    unfixed_templ = jinja2.Template(UNFIXED_TEMPLATE)
+    unfixed_out = unfixed_templ.render(config)
+    with open("unfixed_snp_calling.sh", "w") as outfile:
+        outfile.write(unfixed_out)
